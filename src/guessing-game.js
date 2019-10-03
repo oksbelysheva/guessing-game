461f13ae465b92d28a96f9fead20d2316de42b12
Oksana Belysheva <oks.belysheva@yandex.ru>
2019-10-03 10:56:01 +0400

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.range = [];
+        this.l = 0;
+        this.r = 0;
+        this.m = (this.l + this.r)/2;
+    }
 
     setRange(min, max) {
-
+        this.range = [];
+        for (let i = min; i <= max; i++) {
+            this.range.push(i);
+        }
+        this.r = this.range.length - 1;
     }
 
     guess() {
-
+        this.m = Math.round((this.l + this.r)/2);
+        return this.range[this.m];
     }
 
     lower() {
-
+        this.r = this.m;
     }
 
     greater() {
-
+        this.l = this.m;
     }
 }
 
